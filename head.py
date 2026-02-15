c56aa4baa0d177b10e88a1d2dc9b46f47d37bf43
xiao mingg <minhnguyen47431@gmail.com>
2026-02-15 11:19:38 +0700

    fix

--- head.py
+++ head.py
@@ -10,18 +10,20 @@ class AdaFace(nn.Module):
                  m=0.4,
                  h=0.333,
                  s=64.,
-                 t_alpha=1.0,
+                 t_alpha=0.99,
                  ):
         super(AdaFace, self).__init__()
         self.classnum = classnum
-        self.kernel = nn.Parameter(torch.Tensor(embedding_size,classnum))
+        self.kernel = nn.Parameter(torch.Tensor(embedding_size, classnum))
 
         # initial kernel
-        self.kernel.data.uniform_(-1, 1).renorm_(2,1, 1e-5).mul_(1e5)
+        nn.init.xavier_uniform_(self.kernel)
         self.m = m
         self.eps = 1e-3
         self.h = h
         self.s = s
+        self.th = np.cos(np.pi - m)
+        self.mm = np.sin(np.pi - m) * m
 
         # ema prep
         self.t_alpha = t_alpha
@@ -38,8 +40,9 @@ class AdaFace(nn.Module):
     def forward(self, embbedings, norms, label):
 
         kernel_norm, _ = l2_norm(self.kernel,axis=0)
-        cosine = torch.mm(embbedings,kernel_norm)
+        cosine = torch.mm(embbedings, kernel_norm) # NxD x DxC -> NxC
         cosine = cosine.clamp(-1 + self.eps, 1 - self.eps)
+        sine = torch.sqrt(1 - cosine**2)
 
         safe_norms = torch.clip(norms, min=0.001, max=100)
         safe_norms = safe_norms.clone().detach()
@@ -50,26 +53,18 @@ class AdaFace(nn.Module):
             self.batch_mean = mean * self.t_alpha + (1 - self.t_alpha) * self.batch_mean
             self.batch_std =  std * self.t_alpha + (1 - self.t_alpha) * self.batch_std
 
-        margin_scaler = (safe_norms - self.batch_mean) / (self.batch_std+self.eps)
+        margin_scaler = (safe_norms - self.batch_mean) / (self.batch_std + self.eps)
         margin_scaler = margin_scaler * self.h
         margin_scaler = torch.clip(margin_scaler, -1, 1)
 
-        # g_angular
-        m_arc = torch.zeros(label.size()[0], cosine.size()[1], device=cosine.device)
-        m_arc.scatter_(1, label.reshape(-1, 1), 1.0)
-        g_angular = self.m * margin_scaler * -1
-        m_arc = m_arc * g_angular
-        theta = cosine.acos()
-        theta_m = torch.clip(theta + m_arc, min=self.eps, max=np.pi-self.eps)
-        cosine = theta_m.cos()
-
-        # g_additive
-        m_cos = torch.zeros(label.size()[0], cosine.size()[1], device=cosine.device)
-        m_cos.scatter_(1, label.reshape(-1, 1), 1.0)
-        g_add = self.m + (self.m * margin_scaler)
-        m_cos = m_cos * g_add
-        cosine = cosine - m_cos
-
-        # scale
-        scaled_cosine_m = cosine * self.s
-        return scaled_cosine_m
+        one_hot = torch.zeros_like(cosine, device=cosine.device) # N x C
+        one_hot.scatter_(1, label.reshape(-1, 1), 1.0)
+        
+        g_angular = -1 * self.m * margin_scaler
+        g_add = self.m * margin_scaler + self.m
+        
+        phi = cosine * torch.cos(g_angular) - sine * torch.sin(g_angular) # phi = cos(theta + ....)
+        phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+        
+        logits = (one_hot * phi) - (one_hot * g_add) + (1 - one_hot) * cosine
+        return logits * self.s
