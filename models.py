55f5b8ad12820c24822f7149a508719e37b1f36f
xiao ming <minhnguyen47431@gmail.com>
2026-03-14 19:55:58 +0700

    add function

---
+++ models.py
@@ -0,0 +1,8 @@
+import torch
+import net
+
+def get_model(name, checkpoint):
+    model = net.build_model(name)
+    state_dict = torch.load(checkpoint)
+    model.load_state_dict(state_dict)
+    return model
